3b24909ea2a78ea7246744409fedbe6c0df53ce4
ruffineEliane <92789108+ruffineEliane@users.noreply.github.com>
2022-06-06 20:17:23 +0300

    Update ChangePasswordType.php

--- src/Form/Type/ChangePasswordType.php
+++ src/Form/Type/ChangePasswordType.php
@@ -3,8 +3,7 @@
 /*
  * This file is part of the Symfony package.
  *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
+ * (c) Eliane Ruffine
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
